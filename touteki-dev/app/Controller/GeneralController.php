df19a34e4a123eff63ab1aca2644bb8bcc53c3e6
甲斐達也 <kaitatsuya@kaitatsuyanoMacBook-Air.local>
2017-09-30 21:12:17 +0900

    修正します

--- touteki-dev/app/Controller/GeneralController.php
+++ touteki-dev/app/Controller/GeneralController.php
@@ -8,39 +8,39 @@ App::uses('AppController', 'Controller');
  */
  class GeneralController extends AppController {
      public $components = [
-        'Session',
-        'Auth' => [
-            // 認証時の設定
-            'authenticate' => [
-                'Form' => [
-                    // 認証時に使用するモデル
-                    'userModel' => 'Customer',
-                    // 認証時に使用するモデルのユーザ名とパスワードの対象カラム
-                    'fields' => [
-                        'username' => 'email',
-                        'password' => 'password'
-                    ]
-                ]
-            ],
-            // ログイン失敗時に出力するメッセージを設定
-            'loginError' => 'パスワードもしくはEmailをご確認ください。',
-            // ログインしていない場合のメッセージを設定
-            'authError' => 'ご利用されるにはログインが必要です。',
-            // ログインに使用するアクションを指定
-            'loginAction' => [
-                'controller' => 'Customers',
-                'action' => 'login'
-            ],
-            // ログイン後のリダイレクト先を指定
-            'loginRedirect' => [
-                'controller' => 'Customers',
-                'action' => 'top'
-            ],
-            // ログアウト後のリダイレクト先を指定
-            'logoutRedirect' => [
-                'controller' => 'Customers',
-                'action' => 'login']
-        ]
+        'Session'
+        // 'Auth' => [
+        //     // 認証時の設定
+        //     'authenticate' => [
+        //         'Form' => [
+        //             // 認証時に使用するモデル
+        //             'userModel' => 'Customer',
+        //             // 認証時に使用するモデルのユーザ名とパスワードの対象カラム
+        //             'fields' => [
+        //                 'username' => 'email',
+        //                 'password' => 'password'
+        //             ]
+        //         ]
+        //     ],
+        //     // ログイン失敗時に出力するメッセージを設定
+        //     'loginError' => 'パスワードもしくはEmailをご確認ください。',
+        //     // ログインしていない場合のメッセージを設定
+        //     'authError' => 'ご利用されるにはログインが必要です。',
+        //     // ログインに使用するアクションを指定
+        //     'loginAction' => [
+        //         'controller' => 'Customers',
+        //         'action' => 'login'
+        //     ],
+        //     // ログイン後のリダイレクト先を指定
+        //     'loginRedirect' => [
+        //         'controller' => 'Customers',
+        //         'action' => 'top'
+        //     ],
+        //     // ログアウト後のリダイレクト先を指定
+        //     'logoutRedirect' => [
+        //         'controller' => 'Customers',
+        //         'action' => 'login']
+        // ]
      ];
 
      public $helpers = [
